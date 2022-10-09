450dbd80c0bedc22b53cff73badaa25cb174b12d
VaclavMatula <76968241+VaclavMatula@users.noreply.github.com>
2022-10-09 22:15:25 +0200

    Delete data2.py

--- data2.py
+++
@@ -1,4 +0,0 @@
-a = {"name" : "Kingsman: The Secret Service", "year" : "2014", "genre" : "action", "county" : "United Kingdom"}
-b = {"name" : "The Lord of the Rings: The Fellowship of the Ring", "year" : "2001", "genre" : "fantasy", "county" : "New Zealand"}
-print(a)
-print(b)
