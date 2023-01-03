447b7f7c92c988974fc1b853cbe664b5a52f61e7
VaclavMatula <76968241+VaclavMatula@users.noreply.github.com>
2023-01-04 00:29:48 +0100

    Update f3.php

--- math-api/f3.php
+++ math-api/f3.php
@@ -1,26 +1,45 @@
 <?php
 $result = 0;
-$numbers = explode(",", filter_input(INPUT_GET, "numbers"));
+$error = "";
 
-if($_GET['method'] == 'sum')
-{
-foreach($numbers as $number){
-  $result += $number;
-}
-}
-if($_GET['method'] == 'sub')
-{
-$result = $numbers[0]-$numbers[1];
+$operation = $_SERVER['REQUEST_URI'];
+$operation = str_replace("/php/math-api.php/", "", $operation);
+
+$json = file_get_contents('php://input');
+$obj = json_decode($json);
+$numbers = $obj->numbers;
+
+if ($operation == 'sum') {
+    foreach($numbers as $number){
+        $result += $number;
+    }
+} else if ($operation == 'sub') {
+    $result = $numbers[0];
+    foreach ($numbers as $number) {
+        $result -= $number;
+    }
+} else if ($operation == 'div') {
+    if (count($numbers) < 2) {
+        $error = "Not enough numbers provided for division.";
+    } else {
+        $result = $numbers[0];
+        foreach ($numbers as $number) {
+            $result /= $number;
+        }
+    }
+} else if ($operation == 'mul') {
+    $result = 1;
+    foreach ($numbers as $number) {
+        $result *= $number;
+    }
+} else {
+    $error = "Invalid operation.";
 }
 
-if($_GET['method'] == 'div')
-{
-$result = $numbers[0]/$numbers[1];
+if ($error) {
+    $reply = ["report" => "Error", "error" => $error];
+} else {
+    $reply = ["report" => "OK", "result" => $result];
 }
-if($_GET['method'] == 'mul')
-{
-$result = $numbers[0]*$numbers[1];
-}
-$reply = ["report" => "OK", "result" => $result];
 echo json_encode($reply);
 ?>
